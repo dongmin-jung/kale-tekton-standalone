e3656f082bb509b54dc8ceeae06cb9e644f0edc3
dongmin-jung <dongmin_jung@tmax.co.kr>
2022-02-04 16:47:47 +0900

    test

--- labextension/src/widgets/cell-metadata/CellMetadataEditorTfConfigDialog.tsx
+++ labextension/src/widgets/cell-metadata/CellMetadataEditorTfConfigDialog.tsx
@@ -140,8 +140,8 @@ export const CellMetadataEditorTfConfigDialog: React.FunctionComponent<ICellMeta
                 variant="standard"
                 label="Strategy"
                 values={[
-                  'MultiWorkerMirroredStrategy',
-                  'ParameterServerStrategy'
+                  {label:'MultiWorkerMirroredStrategy', value:'MultiWorkerMirroredStrategy'},
+                  {label:'ParameterServerStrategy', value:'ParameterServerStrategy'}
                 ]}
                 value={distribute === '' ? null : distribute === 'MultiWorkerMirroredStrategy' ? 'MultiWorkerMirroredStrategy' : distribute === 'ParameterServerStrategy' ? 'ParameterServerStrategy' : null}
                 // index={-1}
